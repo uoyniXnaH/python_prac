19747623d5221876b1859132a63e7f9322b5b0b2
Xinyou Han <b4baker@gmail.com>
2020-03-14 16:00:54 +0900

    Update ascii_art.py

--- ascii_art/ascii_art.py
+++ ascii_art/ascii_art.py
@@ -7,12 +7,13 @@ imgFile = Image.open('D:/imgIn.png')
 imgSize = imgFile.size
 imgWid = imgSize[0]
 imgHgt = imgSize[1]
-imgRaw = np.asarray(imgFile.convert('L')).astype('float')
+imgObj = imgFile.resize((imgWid,int(imgHgt/2)))
+imgRaw = np.asarray(imgObj.convert('L')).astype('float')
 imgSym = [i/16 for i in imgRaw]
 imgSym = np.array(np.round(imgSym)).astype('int')
 
 imgOut = open('D:/imgOut.txt','w+')
-for i in range(imgHgt):
+for i in range(int(imgHgt/2)):
 	for j in range(imgWid):
 		imgOut.write(symList[int(imgSym[i][j])])
 	imgOut.write('\n')
